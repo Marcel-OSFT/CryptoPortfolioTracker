7d06fca0a3401bae06d3f7671657ef31676cf914
Marcel-OSFT <141166641+Marcel-OSFT@users.noreply.github.com>
2024-06-19 17:12:54 +0200

    .

--- App.xaml.cs
+++ App.xaml.cs
@@ -107,6 +107,10 @@ public partial class App : Application
                 }
                 File.Copy(dbFilename, appDataPath + "\\sqlCPT_backup_"  + DateTime.Now.Ticks.ToString() + ".db");
             }
+            else
+            {
+                context?.Database.Migrate();
+            }
             //context?.Database.Migrate();
         }
         catch (Exception ex)
