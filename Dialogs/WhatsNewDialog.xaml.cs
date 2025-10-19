2e88ae4f0466d33cc8c135c34e8d43425b9f967d
Marcel-OSFT <141166641+Marcel-OSFT@users.noreply.github.com>
2025-10-19 09:21:57 +0200

    .

--- Dialogs/WhatsNewDialog.xaml.cs
+++ Dialogs/WhatsNewDialog.xaml.cs
@@ -1,19 +1,24 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using CryptoPortfolioTracker.Services;
 using Microsoft.UI.Xaml.Controls;
 using System;
 
 namespace CryptoPortfolioTracker.Dialogs;
 
+[ObservableObject]
 public sealed partial class WhatsNewDialog : ContentDialog
 {
     private const string PdfUrl_NL = "https://marcel-osft.github.io/CryptoPortfolioTracker/docs/WhatsNew_NL.pdf";
     private const string PdfUrl_EN = "https://marcel-osft.github.io/CryptoPortfolioTracker/docs/WhatsNew_EN.pdf";
     private readonly IPreferencesService _preferenceService;
 
+    [ObservableProperty] private Uri pdfDoc;
+
     public WhatsNewDialog(IPreferencesService preferencesService)
     {
         InitializeComponent();
         _preferenceService = preferencesService;
+        //pdfDoc = new Uri(_preferenceService.GetAppCultureLanguage() == "nl" ? PdfUrl_NL : PdfUrl_EN);
     }
 
     private async void WhatsNewDialog_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
